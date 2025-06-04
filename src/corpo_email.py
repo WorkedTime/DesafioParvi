bb5bf313559a858396a1662541874841f6adae1f
Caio Ygor de Souza Silva <emailparaenviodeteste@gmail.com>
2025-06-04 16:12:33 -0300

    Parvi commit att

--- src/corpo_email.py
+++ src/corpo_email.py
@@ -2,6 +2,7 @@ from src.processando_dados import processando_dados
 
 def msg_corpo_email(total_dicionario):
     
-    mensagem = f"Mensagem do corpo do e-mail: O Total de citações é de {total_dicionario["Citacoes"]}\n O autor mais recorrente é o: {total_dicionario["Autor"]}\n E a tag mais repetida é a: {total_dicionario["Tag"]}"
+    #Mensagem do corpo do e-mail
+    mensagem = f"Olá, seguem os dados após análise de conteúdo visto via Web e processado via Pandas:\n\n\nO Total de citações é de {total_dicionario["Citacoes"]} Citações \nO autor mais recorrente é o: {total_dicionario["Autor"]}\nE a tag mais repetida é a: {total_dicionario["Tag"]}"
 
     return mensagem
